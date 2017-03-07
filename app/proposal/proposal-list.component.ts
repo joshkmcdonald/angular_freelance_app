211b1df9bc458148b60491b17cca5fe5126d4459
joshkmcdonald <joshkmcdonald@gmail.com>
2017-03-07 15:21:31 -0700

    Create api connector for proposals and map data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.css']
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
 
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://google.com', 'Ruby on Rails', '10', '700', 1, 'jo@jojo.com')
-  proposalTwo: Proposal = new Proposal(60, 'ABC X', 'http://google.com', 'Ruby on Rails', '10', '700', 1, 'jo@jojo.com')
-  proposalThree: Proposal = new Proposal(90, 'ABC Y', 'http://google.com', 'Ruby on Rails', '10', '700', 1, 'jo@jojo.com')
+  constructor(
+    private proposalService: ProposalService
+  ) {}
 
-  proposals: Proposal[] = [ 
-    this.proposalOne, 
-    this.proposalTwo, 
-    this.proposalThree 
-  ]
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
